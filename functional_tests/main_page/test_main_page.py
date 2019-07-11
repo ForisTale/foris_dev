e3d4e24defe04bd8f4a00509b8f298223351900f
Foris <foris4@gmail.com>
2019-07-11 17:06:34 +0100

    Connect main page with lists.

--- functional_tests/main_page/test_main_page.py
+++ functional_tests/main_page/test_main_page.py
@@ -3,6 +3,12 @@ from .base import FunctionalTest
 
 class BasicTest(FunctionalTest):
 
+    def check_link_send_to_correct_url(self, link_text, url_ending):
+        self.driver.find_element_by_link_text(link_text).click()
+        current_url = self.driver.current_url
+        desired_url = self.live_server_url + url_ending
+        self.assertEqual(current_url, desired_url)
+
     def test_has_basic_functionality(self):
         # Foris visit his new website.
         self.driver.get(self.live_server_url)
@@ -17,9 +23,7 @@ class BasicTest(FunctionalTest):
         self.assertIn("About Me", links)
 
         # He click on about me
-        self.driver.find_element_by_link_text("About Me").click()
-        desired_location = self.live_server_url + "/about_me"
-        self.assertEqual(desired_location, self.driver.current_url)
+        self.check_link_send_to_correct_url("About Me", "/about_me")
 
         # There is basic description about him
         description = self.driver.find_element_by_id("about_me").text
@@ -34,3 +38,13 @@ class BasicTest(FunctionalTest):
         # Satisfied he click on main site link and is back to main site.
         self.driver.find_element_by_link_text("Foris.dev").click()
         self.assertEqual(self.driver.current_url, self.live_server_url + "/")
+
+    def test_can_go_to_superlists_and_back(self):
+        # Foris visit home page
+        self.driver.get(self.live_server_url)
+
+        # Hi spotted new link to superlist, decided to visit it.
+        self.check_link_send_to_correct_url("Superlists", "/lists/")
+
+        # Everything looks ok so he come back to main site by link.
+        self.check_link_send_to_correct_url("Foris.dev", "/")
