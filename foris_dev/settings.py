06d8f4e7c468e11746d31a4293b1e8b1664531cf
Foris <foris4@gmail.com>
2019-07-22 13:25:05 +0100

    Disable redundant setting and change to site text.

--- foris_dev/settings.py
+++ foris_dev/settings.py
@@ -166,19 +166,21 @@ if is_deployed:
     # security tweaks from:
     # https://github.com/benyaminsalimi/Secure-Headers/blob/master/example/Django_Settings.py
 
-    ## X-XSS-Protection
+    # X-XSS-Protection
     SECURE_BROWSER_XSS_FILTER = True
-    ## X-Frame-Options
+    # X-Frame-Options
     X_FRAME_OPTIONS = 'DENY'
-    #X-Content-Type-Options
+    # X-Content-Type-Options
     SECURE_CONTENT_TYPE_NOSNIFF = True
-    ## Strict-Transport-Security
-    SECURE_HSTS_SECONDS = 15768000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
 
-    ## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
-    SECURE_SSL_REDIRECT = True
+    # These are redundant thanks to .dev domain.
+    # Strict-Transport-Security
+    # SECURE_HSTS_SECONDS = 15768000
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_HSTS_PRELOAD = True
+
+    # that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+    # SECURE_SSL_REDIRECT = True
 
     # for more security
     CSRF_COOKIE_SECURE = True
